9d99a92f0f5618efba991d5144499cde3ebc95bd
jack0wsky <jack0wsky@users.noreply.github.com>
2020-07-18 19:30:16 +0200

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SEO from '~/components/seo'
+import Layout from '../layouts'
 
 const IndexPage = () => (
-  <>
+  <Layout>
+    <h2>work</h2>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-  </>
+  </Layout>
 )
 
 export default IndexPage
