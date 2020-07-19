d664da189aa6df9c5d1d64fe29e064df880a291f
jack0wsky <jack0wsky@users.noreply.github.com>
2020-07-19 10:54:28 +0200

    trailer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,45 +1,55 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { createGlobalStyle } from "styled-components"
+import { Helmet } from "react-helmet"
 
-import Header from "./header"
-import "./layout.css"
+const Global = createGlobalStyle`
+  *, *:before, *:after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Aktiv Grotesk Ex", sans-serif;
+  }
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    {
+      allSite {
+        edges {
+          node {
+            siteMetadata {
+              author
+              description
+              title
+            }
+          }
         }
       }
     }
   `)
-
+  const {
+    allSite: { edges },
+  } = data
+  console.log(edges)
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Global />
+      {edges.map(node => {
+        console.log(node)
+        return (
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>{node.node.siteMetadata.title}</title>
+            <meta
+              name="description"
+              content={node.node.siteMetadata.description}
+            />
+          </Helmet>
+        )
+      })}
+      {children}
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,195 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useEffect, useRef } from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import stars from "../assets/stars.svg"
+import logo from "../assets/logo.svg"
+import gsap from "gsap"
+import { CSSPlugin } from "gsap/CSSPlugin"
+gsap.registerPlugin(CSSPlugin)
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const Wrapper = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const Container = styled.div`
+  width: 60vw;
+  height: 80vh;
+  display: flex;
+  flex-flow: column;
+  -webkit-flex-flow: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: space-between;
+  -webkit-justify-content: space-between;
+  position: relative;
+`
+const Title = styled.h1`
+  font-size: 4em;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: 700;
+  opacity: 0;
+  transform: translateY(30px);
+`
+const Footer = styled.div`
+  width: 50%;
+  height: auto;
+  display: flex;
+  justify-content: space-between;
+`
+const Logo = styled.img`
+  width: 8vw;
+`
+const SM = styled.div`
+  width: 45%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  -webkit-justify-content: space-between;
+  align-items: center;
+  -webkit-align-items: center;
+`
+const Label = styled.p`
+  font-size: 0.8em;
+  text-transform: uppercase;
+  color: #fff;
+`
+const Link = styled.a`
+  transition: 0.3s ease-in-out;
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.3s ease-in-out;
+  }
+`
+
+const IndexPage = () => {
+  const title = useRef()
+  const stars = useRef()
+  const footer = useRef()
+  useEffect(() => {
+    const tl = gsap.timeline({ duration: 2.5 })
+    tl.to(title.current, {
+      opacity: 1,
+      translateY: 0,
+      duration: 1,
+    })
+    tl.from(stars.current.children, {
+      duration: 0.5,
+      y: 30,
+      opacity: 0,
+      stagger: 0.1,
+    })
+    tl.from(footer.current.children, {
+      y: 40,
+      duration: 1,
+      opacity: 0,
+      stagger: 0.2,
+    })
+  })
+  return (
+    <Layout>
+      <Wrapper>
+        <Container>
+          <Title ref={title}>Już wkrótce</Title>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={"10vw"}
+            viewBox="0 0 554.99 191.99"
+          >
+            <g id="Warstwa_2" dataName="Warstwa 2">
+              <g id="Warstwa_1-2" ref={stars} dataName="Warstwa 1">
+                <path
+                  d="M0,32.66,5.06,17.71l25.3,9.89V0H46V27.6l25.07-9.89,5.52,14.95L50.37,41.4,66.47,63,54.28,72,38,49.45,22.31,72,9.43,63,25.53,41.4Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M119.6,32.66l5.06-14.95L150,27.6V0H165.6V27.6l25.07-9.89,5.52,14.95L170,41.4,186.07,63l-12.19,9L157.55,49.45,141.91,72,129,63l16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M239.2,32.66l5.06-14.95,25.3,9.89V0H285.2V27.6l25.07-9.89,5.52,14.95L289.57,41.4,305.67,63l-12.19,9L277.15,49.45,261.51,72l-12.88-9,16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M358.8,32.66l5.06-14.95,25.3,9.89V0H404.8V27.6l25.07-9.89,5.52,14.95L409.17,41.4,425.27,63l-12.19,9L396.75,49.45,381.11,72l-12.88-9,16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M478.4,32.66l5.06-14.95,25.3,9.89V0H524.4V27.6l25.07-9.89L555,32.66,528.77,41.4,544.87,63l-12.19,9L516.35,49.45,500.71,72l-12.88-9,16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M119.6,152.66l5.06-14.95L150,147.6V120H165.6v27.6l25.07-9.89,5.52,14.95L170,161.4,186.07,183l-12.19,9-16.33-22.54L141.91,192,129,183l16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M239.2,152.66l5.06-14.95,25.3,9.89V120H285.2v27.6l25.07-9.89,5.52,14.95-26.22,8.74L305.67,183l-12.19,9-16.33-22.54L261.51,192l-12.88-9,16.1-21.62Z"
+                  fill="#fff"
+                />
+                <path
+                  d="M358.8,152.66l5.06-14.95,25.3,9.89V120H404.8v27.6l25.07-9.89,5.52,14.95-26.22,8.74L425.27,183l-12.19,9-16.33-22.54L381.11,192l-12.88-9,16.1-21.62Z"
+                  fill="#fff"
+                />
+              </g>
+            </g>
+          </svg>
+          <Footer ref={footer}>
+            <Logo src={logo} />
+            <SM>
+              <Label>Obserwuj nas</Label>
+              <Link
+                href="https://www.instagram.com/fiventhree_clothing/"
+                target="_blank"
+              >
+                <svg
+                  height={"30px"}
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none">
+                    <path
+                      d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"
+                      stroke="#fff"
+                      strokeLinecap="round"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M2 17V7a5 5 0 0 1 5-5h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5z"
+                      stroke="#fff"
+                      strokeWidth="1.5"
+                    />
+                    <path
+                      d="M17.5 6.51l.01-.011"
+                      stroke="#fff"
+                      strokeLinecap="round"
+                      strokeWidth="1.5"
+                    />
+                  </g>
+                </svg>
+              </Link>
+              <Link href="https://www.facebook.com/fiventhree/" target="_blank">
+                <svg
+                  height="25px"
+                  viewBox="0 0 96.124 96.123"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="#fff">
+                    <path d="M72.089.02L59.624 0C45.62 0 36.57 9.285 36.57 23.656v10.907H24.037a1.96 1.96 0 0 0-1.96 1.961v15.803a1.96 1.96 0 0 0 1.96 1.96H36.57v39.876a1.96 1.96 0 0 0 1.96 1.96h16.352a1.96 1.96 0 0 0 1.96-1.96V54.287h14.654a1.96 1.96 0 0 0 1.96-1.96l.006-15.803a1.963 1.963 0 0 0-1.961-1.961H56.842v-9.246c0-4.444 1.059-6.7 6.848-6.7l8.397-.003a1.96 1.96 0 0 0 1.959-1.96V1.98A1.96 1.96 0 0 0 72.089.02z" />
+                  </g>
+                </svg>
+              </Link>
+            </SM>
+          </Footer>
+        </Container>
+      </Wrapper>
+    </Layout>
+  )
+}
 
 export default IndexPage
